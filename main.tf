4b5f1d3af2b35898b37907361e904424034f559f
ganeshdevops66 <ganeshdevops66@gmail.com>
2026-03-14 21:16:48 +0530

    Refactor AWS instance configuration in main.tf
    Removed old webserver instance configuration and added a public instance resource with dynamic tags.

--- main.tf
+++ main.tf
@@ -1,20 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-resource "aws_instance" "webserver" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
-
-  key_name = "aws"
-
-  //vpc_security_group_ids = ["sg-0162dca4bbe268ca0"]
-
-  user_data = file("userdata.sh")
-
-  tags = {
-    Name = "Terraform-WebServer"
-  }
+ tags = {
+   Name = var.name_tag,
+ }
 }
