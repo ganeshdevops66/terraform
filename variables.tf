98b36c716af19b1108e638e57be1f1467bf7122a
ganeshdevops66 <ganeshdevops66@gmail.com>
2026-03-14 21:12:38 +0530

    change

--- variables.tf
+++ variables.tf
@@ -9,3 +9,28 @@ variable "aws_secret_key" {
   type        = string
   default     = ""
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0b6c6ebed2801a5cb"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
